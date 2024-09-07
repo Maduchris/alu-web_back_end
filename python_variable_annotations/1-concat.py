4f1c92d55fcbc7a952cea60a0a78d4c6f0d22fff
Madubuike Christabel Oluomachi <117312670+Maduchris@users.noreply.github.com>
2024-09-07 14:28:16 +0200

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings provided as parameters to the function
+    :param str1: takes a `string`
+    :param str2: a `string`
+    :return: returns a string
+    """
+    return str1 + str2
