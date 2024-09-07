439784ed6f3efbf872941d68eb9b53a9c0fbb4d0
Madubuike Christabel Oluomachi <117312670+Maduchris@users.noreply.github.com>
2024-09-07 14:18:03 +0200

    0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b:float) -> float:
+    """
+     Adds two `float` numbers provided as parameters to the function
+    :param a: a float number
+    :param b: a float number
+    :return: returns a float
+    """
+    return a + b
