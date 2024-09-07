191c8512cb79fd5df1e4c3858d308dc4c5d7bd78
Maduchris <o.madubuike@alustudent.com>
2024-09-07 08:44:03 -0700

    1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
+"""Concat two strings."""
 
 
 def concat(str1: str, str2: str) -> str:
-    """
-    Concatenates two strings provided as parameters to the function
-    :param str1: takes a `string`
-    :param str2: a `string`
-    :return: returns a string
-    """
+    """Concat two strings."""
     return str1 + str2
